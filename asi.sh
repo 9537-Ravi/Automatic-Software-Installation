56067a48223d86bc6111a60fa2e9595c544c123b
9537-Ravi <ravithakar58@gmail.com>
2024-08-02 18:10:03 +0530

    Update asi.sh

--- asi.sh
+++ asi.sh
@@ -326,7 +326,15 @@ done
      sudo mkswap /swapfile
      sudo swapon /swapfile
             ;;
-        
+	    12)
+            echo "Repairing Wired/Wireless Connection..."
+            # Add commands to repair wired/wireless connection
+            echo "Repairing Wired/Wireless Connection..."
+    # Restart network service
+    sudo service network-manager restart
+    # Reset network settings
+    sudo nmcli c delete id'*'
+    ;;  
         13)
             echo "Repairing Wifi Access for Standard User..."
             # Add commands to repair wifi access for standard user
